6af1f088aed3238082f39bc92e14d72cf143dd6f
aloysiustayy <aloysius.tayhm@gmail.com>
2024-04-01 22:58:42 +0800

    Merge branch 'team4'
    merged

--- ICT2112-P3-Project/PresentationLayer/Program.cs
+++ ICT2112-P3-Project/PresentationLayer/Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddDbContext<DataContext>(options =>
 // Register data source layer abstractions to decouple.
 builder.Services.AddScoped<IAdministratorTDG, AdministratorTDG>();
 
+// Team 7
 builder.Services.AddScoped<IMedicalPlanTDG, MedicalPlanTDG>();
 
 builder.Services.AddScoped<IMedicationTrackerTDG, MedicationTrackerTDG>();
@@ -33,6 +34,19 @@ builder.Services.AddScoped<IDrugTDG, DrugTDG>();
 
 builder.Services.AddScoped<IMedicalCounsellingTDG, MedicalCounsellingTDG>();
 
+// Team 4
+builder.Services.AddScoped<PhotoControl>();
+builder.Services.AddScoped<SafetyChecklistControl>();
+builder.Services.AddScoped<CommunicationControl>();
+
+// Register the IPhotoTDG and ISafetyChecklistTDG interfaces with their respective implementations
+builder.Services.AddScoped<IPhotoTDG, PhotoTDG>();
+builder.Services.AddScoped<ISafetyChecklistTDG, SafetyChecklistTDG>();
+
+// Register the CommunicationControl
+builder.Services.AddScoped<ICommunicationTDG, CommunicationTDG>();
+
+builder.Services.AddScoped<IMedicalPlanTDG, MedicalPlanTDG>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
