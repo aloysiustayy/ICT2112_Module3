646758f6ecd6c3f9245f91c18acf29b7f2e3c34c
jacksonhooi <113287892+jacksonhooi@users.noreply.github.com>
2024-04-05 01:53:32 +0800

    clean up code

--- ICT2112-P3-Project/DomainLayer/Entity/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DomainLayer.Entity
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
